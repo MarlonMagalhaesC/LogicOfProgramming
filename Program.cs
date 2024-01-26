455da3cba9926fdf824080c17543e250ae10cc6b
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-01-26 16:24:32 -0300

    oitavocommit

--- Program.cs
+++ Program.cs
@@ -6,25 +6,20 @@ class Program
 {
     static void Main()
     {
-        int n, numeros, conti = 0, contp = 0;
+        int n;
 
-        Console.WriteLine("Digite a quantidade de numeros: ");
+        Console.WriteLine("Digite um numero inteiro positivo: ");
         n = int.Parse(Console.ReadLine());
 
-        for (int i = 1; i <= n; i++)
+          Console.WriteLine($"Os divisores positivos sao: ");
+
+        for (int i=1; i<=n; i++)
         {
-            Console.WriteLine($"Digite o {i}o numero: ");
-            numeros = int.Parse(Console.ReadLine());
+           if (n % i == 0)
+           Console.WriteLine(i);
 
-            if (numeros % 2 == 0)
-                contp++;
-
-            else
-                conti++;
         }
 
-        Console.WriteLine($"A quantidade de numeros impares sao: {conti}");
-        Console.WriteLine($"A quantidade de numeros pares sao: {contp}");
 
 
     }

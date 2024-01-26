23cf5a31947859c396c8c641cd498bcc18000b4f
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-01-26 10:24:03 -0300

    terceiro commit

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
 ﻿using System;
-using System.Diagnostics;
 
 class Program
 {
     static void Main()
     {
-       int q1, q2;
+        string nomecand1, nomecand2;
+        int votoscand1, votoscand2;
 
-        Console.WriteLine("Informe um numero inteiro: ");
-        q1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o nome do candidato 1:");
+        nomecand1 = Console.ReadLine();
 
-          Console.WriteLine("Informe outro numero inteiro: ");
-        q2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o numero de votos do candidato 1:");
+        votoscand1 = int.Parse(Console.ReadLine());  // Correção aqui
 
-        
-ewrwerwew
+        Console.WriteLine("Informe o nome do candidato 2:");
+        nomecand2 = Console.ReadLine();
 
+        Console.WriteLine("Informe o numero de votos do candidato 2:");
+        votoscand2 = int.Parse(Console.ReadLine());  // Correção aqui
 
+        if (votoscand1 > votoscand2)
+            Console.WriteLine($"O candidato {nomecand1} ganhou");
+
+        else if (votoscand2 > votoscand1)
+            Console.WriteLine($"O candidato {nomecand2} ganhou");
+
+        else
+            Console.WriteLine($"Uma nova eleição deve ser realizada, houve empate entre os candidatos");
     }
 }
-

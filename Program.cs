5279007e7266cfb22431fad4a3683efff9d579ef
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-01-26 10:01:49 -0300

    segundo commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World! 2");
+       int q1, q2;
+
+        Console.WriteLine("Informe um numero inteiro: ");
+        q1 = int.Parse(Console.ReadLine());
+
+          Console.WriteLine("Informe outro numero inteiro: ");
+        q2 = int.Parse(Console.ReadLine());
+
+        
+ewrwerwew
+
+
     }
 }
 

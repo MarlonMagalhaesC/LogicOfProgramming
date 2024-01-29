f70725978d42758a9c05854c47cf9a50ddacafcc
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-01-29 16:18:58 -0300

    11commit

--- Program.cs
+++ Program.cs
@@ -1,30 +1,70 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq.Expressions;
 
 class Program
 {
   static void Main()
   {
-    int q1, q2;
+    int n;
 
-    Console.WriteLine("Digite um numero inteiro positivo: ");
-    q1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o numero de eleitores: ");
+    n = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Digite um numero inteiro positivo: ");
-    q2 = int.Parse(Console.ReadLine());
+    if (n < 10)
+      Console.WriteLine($"{n} e menor que 10, digite novamente. ");
 
-    for (int i = 1; i <= q1; i++)
+
+
+    else
     {
-      for (int j = 1; j <= q2; j++)
+      int voto, Teobaldo = 0, Astrogildo = 0, branco = 0, nulo = 0;
+
+      Console.WriteLine($"Tabela de votacao. \n 1- Teobaldo \n 2- Astrogildo \n 0- Branco \n Nulo- Demais numeros.  ");
+
+      for (int i = 1; i <= n; i++)
       {
-        Console.WriteLine($"{i},{j}");
+        voto = 0;
+
+        Console.WriteLine($"{i}o Eleitor, digite o numero do seu voto. ");
+        voto = int.Parse(Console.ReadLine());
+
+        if (voto == 1)
+          Teobaldo++;
+
+        else if (voto == 2)
+          Astrogildo++;
+
+        else if (voto == 0)
+          branco++;
+
+        else
+          nulo++;
+
+
       }
+
+      Console.WriteLine($"O candidato Telbado teve um total de {Teobaldo} votos. ");
+      Console.WriteLine($"O candidato Astrogildo teve um total de {Astrogildo} votos. ");
+      Console.WriteLine($"Votos em branco: {branco} ");
+      Console.WriteLine($"Votos nulos {nulo} ");
+
+
+
+
+
+
+
+
+
+
     }
-
-
-
-
-
   }
 }
+
+
+
+
+
+
+
+
+

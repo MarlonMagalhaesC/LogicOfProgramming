1a07401fb1310ecdc107e0cb283f1c0b2d846ea9
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-01-29 11:23:15 -0300

    oitavocommit

--- Program.cs
+++ Program.cs
@@ -4,23 +4,27 @@ using System.Linq.Expressions;
 
 class Program
 {
-    static void Main()
+  static void Main()
+  {
+    int q1, q2;
+
+    Console.WriteLine("Digite um numero inteiro positivo: ");
+    q1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite um numero inteiro positivo: ");
+    q2 = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= q1; i++)
     {
-        int n;
-
-        Console.WriteLine("Digite um numero inteiro positivo: ");
-        n = int.Parse(Console.ReadLine());
-
-          Console.WriteLine($"Os divisores positivos sao: ");
-
-        for (int i=1; i<=n; i++)
-        {
-           if (n % i == 0)
-           Console.WriteLine(i);
-
-        }
-
-
-
+      for (int j = 1; j <= q2; j++)
+      {
+        Console.WriteLine($"{i},{j}");
+      }
     }
+
+
+
+
+
+  }
 }

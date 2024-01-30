3a810c12f6ea6789abb27bd8de5d50962f8ddc19
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-01-30 17:34:00 -0300

    13commit

--- Program.cs
+++ Program.cs
@@ -1,28 +1,32 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Xml;
 
 class Program
 {
   static void Main()
   {
-    int n, idade, soma=0;
-    double media;
+    int veiculos;
+    float soma = 0, multas;
+    float media;
 
-    Console.WriteLine("Digite um numero de n pessoas: ");
-    n = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite quantos veiculos sao: ");
+    veiculos = int.Parse(Console.ReadLine());
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= veiculos; i++)
     {
-     Console.WriteLine($"Digite a idade da {i}o pessoa: ");
-     idade = int.Parse(Console.ReadLine());
+      Console.WriteLine($"Digite o valor da multa do {i}o veiculo: ");
+      multas = float.Parse(Console.ReadLine());
 
-     soma += idade;
+      soma += multas;
 
     }
+    media = soma/veiculos;
 
-    media = (double)soma/n;
- 
-Console.WriteLine($"A media sera de {media}");
+    Console.WriteLine($"A soma das multas{soma}");
+    Console.WriteLine($"A media das multas{media}");
+    Console.WriteLine($"A quantidade de veiculos {veiculos}");
+
 
 
 

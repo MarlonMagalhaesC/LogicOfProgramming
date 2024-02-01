84e2156bd4ef421d59799daa2975dff42db1c4b7
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-01 12:12:51 -0300

    jeitowhile

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        int x, y;
+        int x, y, produto, adicao;
 
         Console.WriteLine("Digite um numero ");
         x = int.Parse(Console.ReadLine());
@@ -12,14 +12,15 @@ class Program
         Console.WriteLine("Digite um numero ");
         y = int.Parse(Console.ReadLine());
 
-       int diferenca = (x*y)-(x+y);
-       int resultado = x+y;
+        produto = x * y;
+        adicao = x + y;
 
-        for (int i=1; i<=diferenca; i++)
+        // Correção no loop while
+        while (produto != adicao)
         {
-          resultado++;
+            adicao = adicao + 1;
         }
 
-        Console.WriteLine($"{x * y}, {resultado}");
+        Console.WriteLine($"{x * y}, {adicao}");
     }
 }

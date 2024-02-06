b6867db6287e314b6e7e64b7d20ce98314f93261
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-06 10:45:21 -0300

    22

--- Program.cs
+++ Program.cs
@@ -7,54 +7,22 @@ class Program
 {
     static void Main()
     {
-        int n, soma = 0, omaior = int.MinValue, omenor = int.MaxValue, pares = 0, impares = 0, positivos = 0, negativos = 0, nulos = 0;
-        double media;
+        int n;
+        double x;
 
 
-        Console.WriteLine("Digite 4 numeros: ");
+        Console.WriteLine("Digite um numero: ");
+        n = int.Parse(Console.ReadLine());
 
-        for (int i = 1; i <= 4; i++)
+        for (int i = 1; i <= 9; i++)
         {
-            Console.WriteLine($"Digite o {i}o numero: ");
-            n = int.Parse(Console.ReadLine());
+           x = Math.Pow(2,i);
+            if (x < n)
+            Console.WriteLine($"{x} e menor que {n}. ");
 
-            soma += n;
-
-            if (n > omaior)
-                omaior = n;
-
-            if (n < omenor)
-                omenor = n;
-
-            if (n % 2 == 0)
-                pares++;
-
-            else
-                impares++;
-
-            if (n > 0)
-                positivos++;
-
-            else if (n < 0)
-                negativos++;
-
-            else
-                nulos++;
+           
 
         }
-
-        media = (double)soma / 4;
-
-        Console.WriteLine($"A soma sera de: {soma}");
-        Console.WriteLine($"A media sera de: {media}  ");
-        Console.WriteLine($"o maior sera de: {omaior}");
-        Console.WriteLine($"o menor sera de: {omenor}");
-        Console.WriteLine($"pares sera de: {pares}");
-        Console.WriteLine($"impares sera de: {impares}");
-        Console.WriteLine($"positivos sera de: {positivos}");
-        Console.WriteLine($"negativoa sera de: {negativos}");
-        Console.WriteLine($"nulos sera de: {nulos}");
-
 
 
 

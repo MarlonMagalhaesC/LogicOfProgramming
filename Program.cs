76c0f16a545222a5c991375ff84a77bbadb42f79
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-06 09:06:06 -0300

    18

--- Program.cs
+++ Program.cs
@@ -9,14 +9,29 @@ class Program
         Console.WriteLine("Digite um valor inteiro positivo n:");
         n = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Todos os pares ordenados entre (0,0) e (n,n): ");
+        Console.WriteLine("Inteiros positivos: ");
 
-        for (int i = 0; i <= n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            for (int j = 0; j <= n; j++)
-            {
-                Console.WriteLine($"({i},{j})");
-            }
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("Inteiros positivos Pares: ");
+
+        for (int i = 2; i <= n * 2; i++)
+        {
+            if (i % 2 == 0)
+                Console.WriteLine(i);
+        }
+
+
+        
+        Console.WriteLine("Inteiros positivos Impares: ");
+
+        for (int i = 1; i <= n * 2; i++)
+        {
+            if (i % 2 == 1)
+                Console.WriteLine(i);
         }
     }
 }

7c086c456fca4dc9eea34a5cb8a75c6de69e854e
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-06 10:26:59 -0300

    21

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
@@ -6,28 +7,55 @@ class Program
 {
     static void Main()
     {
-        int n, idade, i = 0, soma = 0;
+        int n, soma = 0, omaior = int.MinValue, omenor = int.MaxValue, pares = 0, impares = 0, positivos = 0, negativos = 0, nulos = 0;
         double media;
 
-        Console.WriteLine("Calcule a media da turma");
 
-        do
+        Console.WriteLine("Digite 4 numeros: ");
+
+        for (int i = 1; i <= 4; i++)
         {
-            i++;
-            Console.WriteLine($"Digite a idade da {i}o pessoa: ");
-            idade = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Digite o {i}o numero: ");
+            n = int.Parse(Console.ReadLine());
 
-            if (idade > 0)
-            {
-               soma += idade;
-            }
+            soma += n;
 
-        } while (idade != 0);
+            if (n > omaior)
+                omaior = n;
+
+            if (n < omenor)
+                omenor = n;
+
+            if (n % 2 == 0)
+                pares++;
+
+            else
+                impares++;
+
+            if (n > 0)
+                positivos++;
+
+            else if (n < 0)
+                negativos++;
+
+            else
+                nulos++;
+
+        }
+
+        media = (double)soma / 4;
+
+        Console.WriteLine($"A soma sera de: {soma}");
+        Console.WriteLine($"A media sera de: {media}  ");
+        Console.WriteLine($"o maior sera de: {omaior}");
+        Console.WriteLine($"o menor sera de: {omenor}");
+        Console.WriteLine($"pares sera de: {pares}");
+        Console.WriteLine($"impares sera de: {impares}");
+        Console.WriteLine($"positivos sera de: {positivos}");
+        Console.WriteLine($"negativoa sera de: {negativos}");
+        Console.WriteLine($"nulos sera de: {nulos}");
 
 
-        media = (double)soma / i;
-
-        Console.WriteLine($"A media sera de: {media} ");
 
 
 

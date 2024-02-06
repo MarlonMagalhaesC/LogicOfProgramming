110d3b85e63b9b18d8630469528a87c2daac3cd3
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-06 09:01:31 -0300

    18

--- Program.cs
+++ Program.cs
@@ -4,38 +4,19 @@ class Program
 {
     static void Main()
     {
-        int x, y, z, n;
-        double media;
+        int n;
 
-        do
+        Console.WriteLine("Digite um valor inteiro positivo n:");
+        n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Todos os pares ordenados entre (0,0) e (n,n): ");
+
+        for (int i = 0; i <= n; i++)
         {
-            Console.WriteLine("Digite n: ");
-            n = int.Parse(Console.ReadLine());
-
-            if (n < 5)
-                Console.WriteLine("O n nao pode ser menor que 5. ");
-        } while (n < 5);
-
-
-        for (int i = 1; i <= n; i++)
-        {
-            Console.WriteLine("Digite x: ");
-            x = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite y: ");
-            y = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite z: ");
-            z = int.Parse(Console.ReadLine());
-
-            media = (double)(x + z + y) / 3;
-
-
-            Console.WriteLine($"A media e de {media} ");
-
-
+            for (int j = 0; j <= n; j++)
+            {
+                Console.WriteLine($"({i},{j})");
+            }
         }
-
-
     }
 }

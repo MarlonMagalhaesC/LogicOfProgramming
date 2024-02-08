327e76da8a9f0c640300049f9204ae79850b61ce
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-08 11:17:38 -0300

    loops

---
+++ loops - Copy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        int n, fatorial = 1;
+
+        Console.WriteLine("Digite um numero: ");
+        n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            fatorial *= i;
+
+        }
+        Console.WriteLine($"O fatorial de {n} sera de {fatorial}");
+
+
+
+    }
+}

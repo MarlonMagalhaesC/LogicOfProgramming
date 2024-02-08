45382cbb85e522c8fcfb3993ec61db7d959182d6
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-08 10:04:19 -0300

    29

--- Program.cs
+++ Program.cs
@@ -1,62 +1,29 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main()
     {
-        int n, i;
+        int n, i, numeros, soma = 0;
+        float media;
 
-        Console.WriteLine("Números inteiros de 0 a 50: ");
+        Console.WriteLine("Digite um numero n: ");
+        n = int.Parse(Console.ReadLine());
 
-        for (i = 0; i <= 50; i++)
+        for (i = 1; i <= n; i++)
         {
-            Console.Write($"{i}-");
-        }
-        Console.WriteLine();
+            Console.Write($"Digite o {i}o numero:");
+            numeros = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Números inteiros pares de 0 a 50: ");
+            soma += numeros;
 
-        for (i = 0; i <= 50; i++)
-        {
-            if (i % 2 == 0)
-                Console.Write($"{i}-");
-        }
-        Console.WriteLine();
-
-         Console.WriteLine("Números inteiros múltiplos de 3 de 1 a 50:");
-          for (i = 1; i <= 50; i++)
-        {
-            if (i % 3 == 0)
-                Console.Write($"{i}-");
-        }
-        Console.WriteLine();
-
-          Console.WriteLine("Números inteiros pares e múltiplos de 3 de 0 a 50:");
-          for (i = 1; i <= 50; i++)
-        {
-            if (i % 3 == 0 && i % 2 == 0)
-                Console.Write($"{i}-");
         }
 
-        Console.WriteLine();
+        media = (float)soma / n;
 
-          Console.WriteLine("Números inteiros de 100 a 1 (em ordem decrescente):");
-          for (i = 100; i >= 1; i--)
-        {
-                Console.Write($"{i}-");
-        }
-        Console.WriteLine();
-
-        Console.WriteLine("Quadrado dos números inteiros no intervalo fechado de 1 a 20:");
-
-          for (i = 1; i <= 20; i++)
-        {
-            
-                Console.Write($"{Math.Pow(i,2)}-");
-        }
-
-        
-
+        Console.WriteLine($"A soma sera de: {soma}");
+        Console.WriteLine($"A media sera de: {media}");
 
 
 

28b32c2be3e857c8d819bdebe1ca9c93b1dc9bd8
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-08 10:28:13 -0300

    30

--- Program.cs
+++ Program.cs
@@ -5,22 +5,28 @@ class Program
 {
     static void Main()
     {
-        int n, i, numeros, soma = 0;
+        int i = 1, numeros, soma = 0, flag = 0;
         float media;
 
-        Console.WriteLine("Digite um numero n: ");
-        n = int.Parse(Console.ReadLine());
 
-        for (i = 1; i <= n; i++)
+
+        while (flag != -1)
         {
             Console.Write($"Digite o {i}o numero:");
             numeros = int.Parse(Console.ReadLine());
 
-            soma += numeros;
+            if (numeros >= 0)
+            {
+                soma += numeros;
+                i++;
+            }
+
+            else
+                flag--;
 
         }
 
-        media = (float)soma / n;
+        media = (float)soma / (i - 1);
 
         Console.WriteLine($"A soma sera de: {soma}");
         Console.WriteLine($"A media sera de: {media}");

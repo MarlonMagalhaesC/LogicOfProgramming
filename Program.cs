a453538bd63088958e1dd4918d1e0f9a3758a899
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-09 16:04:15 -0300

    v6

--- Program.cs
+++ Program.cs
@@ -6,22 +6,41 @@ class Program
     {
         int n, cont = 0;
 
-        Console.WriteLine("Digite o número de dezenas que uma determinada pessoa apostou na MegaSena: ");
-        n = int.Parse(Console.ReadLine());
+
+
+        do
+        {
+            Console.WriteLine("Digite o número de dezenas que uma determinada pessoa apostou na MegaSena, (entre 6-15): ");
+            n = int.Parse(Console.ReadLine());
+
+
+        } while (n < 6 || n > 15);
+
+
 
         int[] VA = new int[n];
         int[] VS = new int[6];
 
         for (int i = 0; i < VA.Length; i++)
         {
-            Console.WriteLine($"Digite a {i + 1}o dezena que a pessoa apostou: ");
+            Console.WriteLine($"Digite a {i + 1}o dezena que a pessoa apostou: (entre 1 e 60) ");
             VA[i] = int.Parse(Console.ReadLine());
+            if (VA[i] < 1 || VA[i] > 60)
+            {
+                Console.WriteLine("(entre 1 e 60)");
+                i--;
+            }
         }
 
         for (int i = 0; i < VS.Length; i++)
         {
             Console.WriteLine($"Digite a {i + 1}o dezena que foi sorteada: ");
             VS[i] = int.Parse(Console.ReadLine());
+            if (VS[i] < 1 || VS[i] > 60)
+            {
+                Console.WriteLine("(entre 1 e 60)");
+                i--;
+            }
         }
 
         Console.WriteLine("Vetor que a pessoa apostou: ");
@@ -49,7 +68,7 @@ class Program
             }
         }
 
- Console.WriteLine($"A pessoa acertou {cont}dezenas: ");
+        Console.WriteLine($"A pessoa acertou {cont}dezenas: ");
 
 
 

b8eff8ca1d579958857c564f79abb48ceed6c719
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-12 11:46:31 -0300

    15

--- Program.cs
+++ Program.cs
@@ -4,54 +4,27 @@ class Program
 {
     static void Main()
     {
-        int q1, q2, cont = 0;
+        int i, soma = 0;
+        float media;
 
-        Console.WriteLine("Digite um numero: ");
-        q1 = int.Parse(Console.ReadLine());
+        int[] V = new int[10];
 
-        Console.WriteLine("Digite um numero: ");
-        q2 = int.Parse(Console.ReadLine());
-
-        int[] V1 = new int[q1];
-
-        int[] V2 = new int[q2];
-
-        for (int i = 0; i < V1.Length; i++)
+        for (i = 0; i < 10; i++)
         {
-            Console.WriteLine($"Digite o {i + 1}o elemento do vetor 1: ");
-            V1[i] = int.Parse(Console.ReadLine());
-
-            for (int j=0; j<i; j++)
-            {
-              if (V1[i] == V1[j])
-              {
-                Console.WriteLine("Nao e permitido a repeticao de numeros: ");
-                i--;
-              }
-
-            }
+            Console.WriteLine($"Digite o {i + 1} numero");
+            V[i] = int.Parse(Console.ReadLine());
+            soma += V[i];
         }
 
-        for (int i = 0; i < V2.Length; i++)
+        media = (float)soma / 10;
+        
+        for (i = 0; i < V.Length; i++)
         {
-            Console.WriteLine($"Digite o {i + 1}o elemento do vetor 2: ");
-            V2[i] = int.Parse(Console.ReadLine());
+           if (V[i] > media){
+           Console.WriteLine($"o {i + 1}o elemeento, {V[i]} e maior que a media.");
+           }
         }
 
-        for (int i = 0; i < V1.Length; i++)
-        {
-            for (int j = 0; j < V2.Length; j++)
-            {
-                if (V1[i] == V2[j])
-                    cont++;
-            }
-
-              Console.WriteLine($" {V1[i]} presente {cont}vezes");
-              cont=0;
-
-        }
-
-       
 
     }
 }

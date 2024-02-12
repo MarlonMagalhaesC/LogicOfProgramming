832781b69256f436f3462c2cce650c6126f35d85
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-12 16:42:04 -0300

    v18

--- Program.cs
+++ Program.cs
@@ -5,36 +5,49 @@ class Program
 {
     static void Main()
     {
-        int n, x, cont = 0;
+        int q1, q2, cont = 0;
 
-        Console.WriteLine("Digite um número: ");
-        n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite q1: ");
+        q1 = int.Parse(Console.ReadLine());
 
-        int[] V = new int[n];
+        Console.WriteLine("Digite q2: ");
+        q2 = int.Parse(Console.ReadLine());
 
-        for (int i = 0; i < V.Length; i++)
+
+        int[] V1 = new int[q1];
+        int[] V2 = new int[q2];
+
+        for (int i = 0; i < V1.Length; i++)
         {
-            Console.WriteLine($"Digite o {i + 1} elemento do vetor: ");
-            V[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Digite o {i + 1}o elemento: ");
+            V1[i] = int.Parse(Console.ReadLine());
         }
 
-        Console.WriteLine("Digite um número: ");
-        x = int.Parse(Console.ReadLine());
-
-
-        for (int i = 0; i < V.Length; i++)
+        for (int i = 0; i < V2.Length; i++)
         {
-            if (x == V[i])
+            Console.WriteLine($"Digite o {i + 1}o elemento: ");
+            V2[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("");
+
+        for (int i = 0; i < V1.Length; i++)
+        {
+            for (int j = 0; j < V2.Length; j++)
             {
-                cont++;
+                if (V1[i] > V2[j])
+                    cont++;
             }
+
+            if (cont == V2.Length)
+                Console.WriteLine($"{V1[i]} e maior que todos os elementos de V2.");
+
+                cont = 0;
+
         }
 
-        if (cont > 0)
-                Console.WriteLine($"{x} esta entre os numeros armazenados {cont} vez(es)!");
 
-       else
-            Console.WriteLine($"{x} NAO esta entre os numeros armazenados.");
+
 
 
 

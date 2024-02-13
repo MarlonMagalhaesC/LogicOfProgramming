4327414a650c3bb4874a657fe6a980637cf1577f
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-13 16:00:52 -0300

    v22

--- Program.cs
+++ Program.cs
@@ -1,50 +1,47 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 
 class Program
 {
     static void Main()
     {
 
-        int[] V = new int[10];
+        int n;
+        float media, soma = 0, cont=0;
 
+        Console.WriteLine("Digite um numero: ");
+        n = int.Parse(Console.ReadLine());
 
+        string[] Placas = new string[n];
+        float[] Multas = new float[n];
 
-        for (int i = 0; i < V.Length; i++)
+        for (int i = 0; i < Placas.Length; i++)
         {
-            Console.WriteLine($"Digite o elemento {i + 1}o: ");
-            V[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Digite a placa do {i + 1}o veiculo: ");
+            Placas[i] = Console.ReadLine();
+
+            Console.WriteLine($"Digite a multa do {i + 1}o veiculo: ");
+            Multas[i] = float.Parse(Console.ReadLine());
+
+            if (Multas[i] > 0)
+            {
+                soma += Multas[i];
+                cont++;
+            }
         }
 
-        Console.WriteLine($"Vetor: ");
+        media = soma / cont;
+        Console.WriteLine($"media: {media} ");
 
-        for (int i = 0; i < V.Length; i++)
+        Console.WriteLine($"Possuem a multa superior a media: ");
+        for (int i = 0; i < Multas.Length; i++)
         {
-            Console.WriteLine(V[i]);
+            if (Multas[i] > media)
+            {
+             Console.WriteLine($" Veiculo {i+1}, Placa: {Placas[i]}, {Multas[i]}");
+            }
         }
-        Console.WriteLine($"");
-
-        for (int i = 0; i < V.Length - 1; i++)
-
-        {
-            V[i] = V[i + 1];
-        }
-
-        V[V.Length - 1] = 0;
-
-        
-
-        Console.WriteLine($"Vetor modificado: ");
-        for (int i = 0; i < V.Length; i++)
-        {
-            Console.WriteLine(V[i]);
-        }
-
-
-
-
-
-
-
 
 
 

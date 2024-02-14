b2bf39994e8496f679fb200a07242e2a3ae7a2bd
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-14 09:34:06 -0300

    f01

--- Program.cs
+++ Program.cs
@@ -4,55 +4,56 @@ class Program
 {
     static void Main()
     {
-        int q1, q2, cont = 0, cont2 = 0;
+        int n;
+
+        Console.WriteLine("Digite um numero inteiro n:");
+        n = int.Parse(Console.ReadLine());
+
+        NprimeirosPos(n);
+
+        NprimeirosPares(n);
+
+        NprimeirosImp(n);
 
 
-        Console.WriteLine("Digite o tamanho do vetor 1: ");
-        q1 = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Digite o tamanho do vetor 2: ");
-        q2 = int.Parse(Console.ReadLine());
+    }
 
-        int[] V1 = new int[q1];
-        int[] V2 = new int[q2];
+    static void NprimeirosPos(int n)
+    {
+        int i;
 
-        for (int i = 0; i < V1.Length; i++)
+        Console.WriteLine("Os n primeiros positivos:");
+
+        for (i = 1; i <= n; i++)
         {
-            Console.WriteLine($"Digite o {i + 1}o elemento do vetor 1: ");
-            V1[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine(i);
         }
+    }
 
-        for (int i = 0; i < V2.Length; i++)
+    static void NprimeirosPares(int n)
+    {
+        int i;
+
+        Console.WriteLine("Os n primeiros positivos Pares:");
+
+        for (i = 2; i <= n * 2; i++)
         {
-            Console.WriteLine($"Digite o {i + 1}o elemento do vetor 2: ");
-            V2[i] = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+                Console.WriteLine(i);
         }
+    }
 
-        for (int i = 0; i < V1.Length; i++)
+
+    static void NprimeirosImp(int n)
+    {
+        int i;
+
+        Console.WriteLine("Os n primeiros positivos Impares:");
+        for (i=1; i<=n*2; i++)
         {
-            for (int j = 0; j < V2.Length; j++)
-            {
-                if (V1[i] % V2[j] == 0)
-                    cont++;
-            }
-
-            if (cont == V2.Length)
-            {
-                Console.WriteLine($"{V1[i]}e multiplo de todos os elementos de V2: ");
-                cont2++;
-            }
-
-            cont = 0;
+          if (i % 2 == 1)
+                Console.WriteLine(i);
         }
-
-        if (cont2 == 0)
-            Console.WriteLine("Nenhum elemento de V1 e multiplo de todos os elementos de V2");
-
-
-
-
-
-
-
     }
 }

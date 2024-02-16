84d6814605d6d90859a8cb3387adebc65639fb55
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-16 16:45:25 -0300

    f13(certo)

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ class Program
     static void Main()
     {
         float a, b, c;
+        double delta;
+
 
         Console.WriteLine("Digite a: ");
         a = float.Parse(Console.ReadLine());
@@ -15,11 +17,7 @@ class Program
         Console.WriteLine("Digite c: ");
         c = float.Parse(Console.ReadLine());
 
-        Console.WriteLine(CalculaDelta(a,b,c));
-
-        
-
-
+        raizequacao(a,b,c);
 
     }
 
@@ -29,7 +27,29 @@ class Program
 
         delta = (double)Math.Pow(b, 2) - (4 * a * c);
 
-       return delta;
+        return delta;
+    }
+
+    static void raizequacao(float a, float b, float c)
+    {
+        double delta, x1, x2;
+
+        delta = CalculaDelta(a, b, c);
+
+        if (delta < 0)
+            Console.WriteLine("Nao existe raiz real para essa equacao: ");
+
+
+        else
+        {
+            x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            Console.WriteLine($"X1 de {a}, {b}, {c}: {x1}");
+            Console.WriteLine($"X2 de {a}, {b}, {c}: {x2}");
+        }
+
+
     }
 
 

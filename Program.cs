b2ced41741280b9cb44cfdea426d515b6d532a1f
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-16 16:28:02 -0300

    f12(certo)

--- Program.cs
+++ Program.cs
@@ -17,26 +17,19 @@ class Program
 
         Console.WriteLine(CalculaDelta(a,b,c));
 
+        
+
 
 
     }
 
     static double CalculaDelta(float a, float b, float c)
     {
-        double delta, x1, x2;
+        double delta;
 
         delta = (double)Math.Pow(b, 2) - (4 * a * c);
 
-        x1 = (-b + Math.Sqrt(delta)) / (2 * a);
-
-        x2 = (-b - Math.Sqrt(delta)) / (2 * a);
-
-        if (x1 > 0)
-            return x1;
-
-        else
-            return x2;
-
+       return delta;
     }
 
 

5f36058558abbb50ce4ef317d44ba16a89d65a1a
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-17 15:56:25 -0300

    f25

--- Program.cs
+++ Program.cs
@@ -1,26 +1,16 @@
-﻿using System.Net.Mail;
+﻿using System.IO.Compression;
+using System.Net.Mail;
 
 class Program
 {
     static void Main()
     {
-        int x,y,z;
-
-        Console.WriteLine("Digite x: ");
-        x = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Digite y: ");
-        y = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Digite z: ");
-        z = int.Parse(Console.ReadLine());
-
-        Console.WriteLine(ContaMultiplos(x,y,z));
-
+        Pergunta();
     }
 
     static int ContaMultiplos(int x, int y, int z)
     {
+
         int i, cont = 0;
 
         for (i = x; i <= y; i++)
@@ -32,8 +22,29 @@ class Program
         return cont;
     }
 
+    static void Pergunta()
+
+    {
+        int a, b, c;
+
+        Console.WriteLine("Digite a: ");
+        a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite b: ");
+        b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite c: ");
+        c = int.Parse(Console.ReadLine());
+
+        if (a < b)
+            Console.WriteLine(ContaMultiplos(a, b, c));
+
+        else 
+        Console.WriteLine("Os valores de a e b não permitem realizar a tarefa.");
 
 
+
+    }
 
 
 

2fee93a05290479bc841a952372a3f4315904e58
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-17 16:14:46 -0300

    f27

--- Program.cs
+++ Program.cs
@@ -5,21 +5,56 @@ class Program
 {
     static void Main()
     {
-       int idade;
+        int n;
 
-       Console.WriteLine("Digite sua idade: ");
-       idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite quantas pessoas serao: ");
+        n = int.Parse(Console.ReadLine());
 
-       Console.WriteLine(TipoPessoa(idade));
+        mostraResultados(n);
+
     }
 
     static int TipoPessoa(int idade)
     {
-     if (idade < 18)
-     return 1;
+        if (idade < 18)
+            return 1;
 
-     else
-     return 2;
+        else
+            return 2;
+    }
+
+    static void mostraResultados(int n)
+    {
+        float media;
+        int somaMenor = 0, SomaMaior = 0, idades, somageral = 0;
+
+
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"Digite a idade da {i} pessoa:");
+            idades = int.Parse(Console.ReadLine());
+
+            somageral += idades;
+
+            if (TipoPessoa(idades) == 1)
+
+            {
+                somaMenor++;
+            }
+
+            else
+                SomaMaior++;
+        }
+        media = (float)somageral / n;
+
+
+        Console.WriteLine("Media geral " +  media);
+        Console.WriteLine("Total menores " +  somaMenor);
+        Console.WriteLine("Total maiores " +  SomaMaior);
+
+
+
     }
 
 

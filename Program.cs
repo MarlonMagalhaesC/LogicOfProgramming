e5f17471b515563fc63266837ba11fc391690779
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-02-17 11:44:28 -0300

    f21

--- Program.cs
+++ Program.cs
@@ -4,52 +4,32 @@ class Program
 {
     static void Main()
     {
-        
-        Console.WriteLine(NDivisoresP(PerguntaN()));
+        int numero;
 
+        numero = LeituraInteiro("Digite um número inteiro:");
+
+        Console.WriteLine($"Número lido: {numero}");
 
 
 
 
     }
 
-    static int PerguntaN()
+    static int LeituraInteiro(string mensagem)
     {
-        int n;
+        int x;
 
-        Console.WriteLine("Digite um numero para que seja n:");
-        n = int.Parse(Console.ReadLine());
+        Console.WriteLine(mensagem);
+        x = int.Parse(Console.ReadLine());
 
-        return n;
-    }
-
-    static int NDivisoresP(int n)
-    {
-
-        if (n < 0)
-        {
-            return -1;
-        }
-
-        else if (n > 0)
-        {
-            int cont = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                if (n % i == 0)
-                    cont++;
-            }
-            return cont;
-        }
-
-        else
-        {
-            return 0;
-        }
-
-
+        return x;
 
     }
+
+
+
+
+
 }
 
 
